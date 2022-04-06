7997040d9e2163852f7728591339a8bac1b7661e
Jeswin James <jpj4@sfu.ca>
2022-04-06 15:15:45 -0700

    Fix gen_hardcoded_noise_class

--- dataset.py
+++ dataset.py
@@ -39,7 +39,7 @@ def gen_hardcoded_noise_class(batch_size, class_num=14, latent_dim = 128):
     noise = torch.randn([batch_size, latent_dim])
     classes = torch.zeros([batch_size, class_num])
     batch_ind = torch.arange(batch_size)
-    class_ind = torch.randint(0, class_num, batch_size)
+    class_ind = torch.randint(0, class_num, [batch_size])
     classes[batch_ind, class_ind] = 1
     np.save(f'hardcoded_class_{batch_size}.npy', classes)
     np.save(f'hardcoded_noise_{batch_size}.npy', noise)
